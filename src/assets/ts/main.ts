f41aac32a432d075d6c906bc1d3bad63ef313988
rik9228 <rank228130@gmail.com>
2022-04-03 22:50:55 +0900

    add：svg-sprite 追加

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,5 +1,10 @@
 import "@/scss/style.scss";
 
+function requireAll(r: any) {
+  r.keys().forEach(r);
+}
+requireAll(require.context("../img/icons/", true, /\.svg$/));
+
 const arr = [1, 2, 3];
 const hoge = "hoge";
 console.log(hoge);
