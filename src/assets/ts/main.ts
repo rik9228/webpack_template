6e2ea5fbab6a1af1d527625447b77199af47f24f
rik9228 <rank228130@gmail.com>
2022-04-04 10:33:08 +0900

    Fix：不要なパッケージを削除

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -3,7 +3,7 @@ import "@/scss/style.scss";
 function requireAll(r: any) {
   r.keys().forEach(r);
 }
-requireAll(require.context("../img/icons/", true, /\.svg$/));
+requireAll(require.context("../img/svg/", true, /\.svg$/));
 
 const arr = [1, 2, 3];
 const hoge = "hoge";
