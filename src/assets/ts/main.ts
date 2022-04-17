e13d987ce71d8703ad4eb6b3d22dee50381f453e
rik9228 <rank228130@gmail.com>
2022-04-17 13:36:26 +0900

    Fix：svgスプライトを非同期で読み込み

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,2 +1,15 @@
 import "@/scss/style.scss";
 import "./lib/smoothScroll";
+import "regenerator-runtime/runtime";
+
+import axios from "axios";
+
+const insertSprite = async () => {
+  const res = await axios.get("/assets/img/svg/sprite.svg");
+  const data = res.data;
+  const body = document.getElementsByTagName("body")[0];
+
+  body.insertAdjacentHTML("afterbegin", data);
+};
+
+insertSprite();
